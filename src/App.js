fe410880d3fb8073fa82b23d3d6919c125562729
Fakito <92991929+HungTouya@users.noreply.github.com>
2025-02-05 16:08:58 +0700

    a

--- src/App.js
+++ src/App.js
@@ -1,36 +1,29 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import Menu from "./pages/Menu";
-import Schedule from "./pages/Schedule";
-import Profile from "./pages/Profile";
-import Login from "./auth/Login";
-import Register from "./auth/Register";
+import React from "react";  
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";  
+import Navbar from "./components/Navbar";  
+import Home from "./pages/Home";         // Placeholder for home page  
+import Menu from "./pages/Menu";         // Menu page to display all recipes  
+import Schedule from "./pages/Schedule"; // Placeholder for schedule page  
+import Profile from "./pages/Profile";    // Placeholder for profile page  
+import RecipePage from "./pages/RecipePage"; // Individual recipe detail page  
+import "./App.css";  
 
-function App() {
-  return (
-    <Router>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/menu">Menu</Link></li>
-          <li><Link to="/schedule">Schedule</Link></li>
-          <li><Link to="/profile">Profile</Link></li>
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/register">Register</Link></li>
-        </ul>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/schedule" element={<Schedule />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </Router>
-  );
-}
+function App() {  
+  return (  
+    <Router>  
+      <div className="App">  
+        <Navbar />  
+        <Routes>  
+          <Route path="/" element={<Home />} />  
+          <Route path="/menu" element={<Menu />} /> {/* Menu Showing All Recipes */}  
+          <Route path="/schedule" element={<Schedule />} />  
+          <Route path="/profile" element={<Profile />} />  
+          <Route path="/recipes/:id" element={<RecipePage />} /> {/* Individual Recipe Detail */}  
+        </Routes>  
+      </div>  
+    </Router>  
+  );  
+}  
 
 export default App;
 
